2d7228fcd30f0845926292fde210c9f077c0cbc6
harshitbhatt <harshitbhatt8723@gmail.com>
2025-01-07 23:25:43 +0530

    Refactor backend server to serve API routes only, update package.json scripts

--- backend/server.js
+++ backend/server.js
@@ -4,8 +4,7 @@ import mcqRoutes from "./routes/mcq.route.js";
 import flashCardRoutes from "./routes/flashCard.route.js";
 import genAiRoutes from "./routes/genAi.route.js";
 import cors from "cors";
-// path is for you to get the path of the directory
-import path from "path";
+
 
 env.config();
 
@@ -15,7 +14,7 @@ const PORT = process.env.PORT || 3000;
 // cors added to allow cross origin requests
 app.use(cors());
 
-const __dirname = path.resolve();
+
 
 // for json data in body
 app.use(express.json());
@@ -24,16 +23,6 @@ app.use("/api/mcq", mcqRoutes);
 app.use("/api/flashCard", flashCardRoutes);
 app.use("/api/generate", genAiRoutes);
 
-// here we are trying to make out dist folder that we will get on build of the frontend through 
-// as our static assets files 
-if(process.env.NODE_ENV === "production"){
-  app.use(express.static(path.join(__dirname,"/frontend/dist")));
-
-  // except the api routes any thing asked from the backend then
-  app.get("*",(req,res)=>{
-    res.sendFile(path.resolve(__dirname,"frontend","dist","index.html"));
-  })
-}
 
 
 app.listen(PORT, () => {
