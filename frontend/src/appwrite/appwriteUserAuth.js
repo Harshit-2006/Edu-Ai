a500e9c08e090e0ab3bccd06e94adc6611b3624b
harshitbhatt <harshitbhatt8723@gmail.com>
2025-01-08 00:21:52 +0530

    change the redirect url of oauth

--- frontend/src/appwrite/appwriteUserAuth.js
+++ frontend/src/appwrite/appwriteUserAuth.js
@@ -48,8 +48,8 @@ export class AuthService {
     try {
        this.account.createOAuth2Session(
         "google",
-        "http://localhost:5173/",
-        "http://localhost:5173/fail"
+        "https://edu-ai-1-tjwy.onrender.com/",
+        "https://edu-ai-1-tjwy.onrender.com/fail"
       );
       // window.location.href = authUrl;
     } catch (error) {
